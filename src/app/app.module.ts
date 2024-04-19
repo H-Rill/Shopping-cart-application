3cef8711e4d9f8e3ddb9523722fa7927fecf7add
harries <harriesrill.b@gmail.com>
2024-04-19 18:05:04 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { CartListComponent } from './component/cart/cart-list/cart-list.componen
 import { CheckoutComponent } from './component/cart/checkout/checkout.component';
 import { CartReducer } from './core/Stores/Cart/Cart.Reducer';
 import { CartEffects } from './core/Stores/Cart/Cart.Effect';
+// import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -69,6 +70,12 @@ import { CartEffects } from './core/Stores/Cart/Cart.Effect';
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() , connectInZone: true}),
     EffectsModule.forRoot([userEffects,AppEffects, productEffects, CustomerEffects,CartEffects]),
     NgxMaskDirective,NgxMaskPipe,
+  //   ServiceWorkerModule.register('ngsw-worker.js', {
+  // enabled: !isDevMode(),
+  // // Register the ServiceWorker as soon as the application is stable
+  // // or after 30 seconds (whichever comes first).
+  // registrationStrategy: 'registerWhenStable:30000'
+// }),
   ],
   providers: [
     provideNgxMask(),
